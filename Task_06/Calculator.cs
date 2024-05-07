7b648fc51c154e97bef88d13dd2b8109a80fbefb
Igor Korzhan <igor.human@gmail.com>
2024-05-07 22:51:31 +0300

    Task 06

---
+++ Task_06/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_06
+{
+    public static class Calculator
+    {
+        // Додавання двох чисел
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        // Віднімання двох чисел
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        // Множення двох чисел
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        // Ділення двох чисел
+        public static double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Ділити на нуль не можна");
+            }
+            return a / b;
+        }
+    }
+}
---
+++ Task_06/Program.cs
@@ -0,0 +1,22 @@
+﻿using Task_06;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double num1 = 15, num2 = 0;
+
+        Console.WriteLine("Додавання: " + Calculator.Add(num1, num2));
+        Console.WriteLine("Віднімання: " + Calculator.Subtract(num1, num2));
+        Console.WriteLine("Множення: " + Calculator.Multiply(num1, num2));
+
+        try
+        {
+            Console.WriteLine("Ділення: " + Calculator.Divide(num1, num2));
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+    }
+}
