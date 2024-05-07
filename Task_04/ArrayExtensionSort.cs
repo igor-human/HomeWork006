9c42b6124c0d74019f076083a87ba383a819f5d2
Igor Korzhan <igor.human@gmail.com>
2024-05-07 22:33:53 +0300

    Task 4

---
+++ Task_04/ArrayExtensionSort.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_04
+{
+    public static class ArrayExtension
+    {
+        public static void SortAscending(this int[] array)
+        {
+            if (array == null) throw new ArgumentNullException(nameof(array));
+            Array.Sort(array);
+        }
+    }
+}
---
+++ Task_04/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using Task_04;
+
+
+namespace _005_Book
+{
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 5, 7, 4, 2, 4, 9, 2 };
+
+            Console.WriteLine(String.Join(", ", numbers));
+
+            numbers.SortAscending();
+
+            Console.WriteLine(String.Join(", ", numbers));
+        }
+    }
+}
