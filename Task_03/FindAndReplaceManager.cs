9e817e0575c1a58f0736afa6a163463c93c8c955
Igor Korzhan <igor.human@gmail.com>
2024-05-07 18:50:51 +0300

    Task 3

---
+++ Task_03/FindAndReplaceManager.cs
@@ -0,0 +1,18 @@
+﻿using _005_Book;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_03
+{
+    static class FindAndReplaceManager
+    {
+        public static void FindNext(string str)
+        {
+            Book book = new Book("Ще одна улюблена книга");
+            book.FindNext(str);
+        }
+    }
+}
---
+++ Task_03/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+using Task_03;
+
+
+namespace _005_Book
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public List<Notes> NotesList { get; set; }
+
+        public Book(string title)
+        {
+            Title = title;
+            NotesList = new List<Notes>();
+        }
+        
+        public void FindNext(string str)
+        {
+            Console.WriteLine("Пошук рядка : " + str);
+        }
+
+        public void AddNote(string str)
+        {
+            var note = new Notes(str);
+            NotesList.Add(note);
+            Console.WriteLine("Замітку додано до книги" + Title + " " + str);
+        }
+
+        public void DisplayNotes()
+        {
+            Console.WriteLine("Замітки для книги " + Title + ":");
+            foreach (var note in NotesList)
+            {
+                Console.WriteLine(" - " +  note.Text);
+            }
+        }
+        public class Notes
+        {
+            public string Text { get; }
+
+            public Notes(string text)
+            {
+                Text = text;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Моя улюблена книга ");
+
+            book.AddNote("Нотатка до книги");
+
+            book.DisplayNotes();
+
+            FindAndReplaceManager.FindNext("слово");
+
+            Console.ReadLine();
+        }
+    }
+}
